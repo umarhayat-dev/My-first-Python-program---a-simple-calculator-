01afcbdf26b661a8079599fad080422291cafd73
Umar hayat <umarhayat.dev@gmail.com>
2025-04-24 13:22:33 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,18 @@
+print('Welcome')
+print('Enter a Number')
+mynum1 = float(input('Enter your first number:')) 
+mynum2 = float(input('Enter your second number:'))
+operation = input('Choose an operation +,-,*,/ : ')
+if operation == '+':
+    print(mynum1 + mynum2)
+elif operation == '-':
+    print(mynum1 - mynum2)
+elif operation == '*':
+    print(mynum1 * mynum2)
+elif operation == '/':
+    if mynum2 == 0:
+        print("Error: Cannot divide by zero.")
+    else:
+        print(mynum1 / mynum2)
+
+
